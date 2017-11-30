63b50beb8b4d20446e7b24324f2c344a4b5e8f2b
szg2008 <l>
2017-11-30 20:00:19 +0800

    ratingselect

---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+    <div class="ratingselect">
+        <div class="rating-type">
+            <span>{{desc.all}}</span>
+            <span>{{desc.positive}}</span>
+            <span>{{desc.negative}}</span>
+        </div>
+        <div class="switch">
+            <span class="icon-check-circle"></span>
+            <span>只看有内容的评价</span>
+        </div>
+    </div>
+</template>
+
+<script>
+const POSITIVE = 0
+const NEGATIVE = 1
+const ALL = 2
+export default {
+    props:{
+        ratings:{
+            type:Array,
+            defalut:[]
+        },
+        selectType:{
+            type:Number,
+            default:ALL
+        },
+        onlyContent:{
+            type:Boolean,
+            default:false
+        },
+        desc:{
+            type:Object,
+            default(){
+                return {
+                    'all':'全部',
+                    'positive':'满意',
+                    'negative':'不满意'
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+    @import '../../common/stylus/mixin'
+    .ratingselect
+        .rating-type
+            padding:18px 0
+            margin:0 18px
+            border-1px(rgba(7,17,27,0.1))
+
+</style>
